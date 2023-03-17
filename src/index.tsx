6d7894eba16cb5a8ba695e1fd69304de3b6854c1
Michael Williams <michael.williams9@ticketmaster.co.uk>
2023-03-17 14:55:54 +0000

    fcc-react-redux: Challenge
    - Use props and events
    
    [demo] [e2e]

--- src/index.tsx
+++ src/index.tsx
@@ -1,66 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const firstBook = {
-  author: "Jordan Moore",
-  title: "Interesting Facts For Curious Minds",
-};
-const secondBook = {
-  author: "James Clear",
-  title: "Atomic Habits",
-};
+const books = [
+  {
+    author: "Jordan Moore",
+    title: "Interesting Facts For Curious Minds",
+    id: 1,
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    id: 2,
+  },
+];
 
 export const BookList = () => {
+  const getBook = (id) => {
+    const book = books.find((book) => book.id === id);
+    console.log(book);
+  };
   return (
     <section className="booklist">
-      <EventExamples />
-      <Book author={firstBook.author} title={firstBook.title} />
-      <Book author={secondBook.author} title={secondBook.title} />
+      {books.map((book) => (
+        <Book {...book} key={book.id} getBook={getBook} />
+      ))}
     </section>
   );
 };
 
-const EventExamples = () => {
-  const handleFormInput = () => {
-    console.log("handle form input");
-  };
-  const handleButtonClick = () => {
-    alert("handle form input");
-  };
-  return (
-    <section>
-      <form>
-        <h2>Typical Form</h2>
-        <input
-          type="text"
-          name="example"
-          onChange={handleFormInput}
-          style={{ margin: "1rem 0" }}
-        />
-      </form>
-      <button onClick={handleButtonClick}>Click Me!</button>
-    </section>
-  );
-};
+const Book = (book) => {
+  const { title, author, getBook, id } = book;
 
-const Book = (author, title) => {
+  const getSingleBook = () => {
+    getBook(id);
+  };
   return (
     <article className="book">
       <h2>{title}</h2>
+      <button onClick={getSingleBook}>Click me!</button>
       <h4>{author}</h4>
     </article>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<BookList />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
